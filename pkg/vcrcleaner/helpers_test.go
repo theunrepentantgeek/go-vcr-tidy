3a87206b087815b039f9f41798de4fc2a1b2a5ed
Bevan Arps <bevan.arps@outlook.com>
2025-12-29 10:11:25 +1300

    Remove common URL prefix in golden files

--- pkg/vcrcleaner/helpers_test.go
+++ pkg/vcrcleaner/helpers_test.go
@@ -13,7 +13,14 @@ import (
 )
 
 func casssetteSummary(cas *cassette.Cassette) string {
+	// Build a summary of the cassette interactions
 	builder := &strings.Builder{}
+
+	prefix := commonURLPrefix(cas)
+	builder.WriteString(
+		fmt.Sprintf(
+			"Common URL prefix: %s\n\n",
+			prefix))
 
 	for _, interaction := range cas.Interactions {
 		// Skip discarded interactions
@@ -27,6 +34,9 @@ func casssetteSummary(cas *cassette.Cassette) string {
 			u = u[:i]
 		}
 
+		// Remove common prefix
+		u = strings.TrimPrefix(u, prefix)
+
 		// Write method and URL
 		fmt.Fprintf(builder,
 			"%s %d %s\n",
@@ -38,6 +48,25 @@ func casssetteSummary(cas *cassette.Cassette) string {
 	return builder.String()
 }
 
+// commonURLPrefix returns the common URL prefix for all interactions in the cassette.
+// This is used to reduce the level of "noise" in golden files.
+func commonURLPrefix(cas *cassette.Cassette) string {
+	if len(cas.Interactions) == 0 {
+		return ""
+	}
+
+	dmp := diffmatchpatch.New()
+
+	prefix := cas.Interactions[0].Request.URL
+	for _, interaction := range cas.Interactions[1:] {
+		u := interaction.Request.URL
+		l := dmp.DiffCommonPrefix(prefix, u)
+		prefix = prefix[:l]
+	}
+
+	return prefix
+}
+
 // newTestLogger creates a test logger for the given test.
 func newTestLogger(t *testing.T) logr.Logger {
 	t.Helper()
@@ -45,7 +74,7 @@ func newTestLogger(t *testing.T) logr.Logger {
 	return testr.NewWithOptions(t, testr.Options{Verbosity: 1})
 }
 
-// DiffPrettyText converts a []Diff into a colored text report.
+// DiffPrettyText converts a []Diff into a text report.
 func diffsToText(diffs []diffmatchpatch.Diff) string {
 	var buff bytes.Buffer
 	for _, diff := range diffs {
